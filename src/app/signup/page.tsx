dd041fcc9aabc8b9cbb3ad1b0de89cfe3a01fcb9
Sarthak Sonawane <sarthak20.sonawane@gmail.com>
2025-07-30 08:22:15 +0530

    signup route

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,25 +1,45 @@
 'use client'
 import Link from 'next/link';
-import React, { use } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-// import{axios} from 'axios';
+import{axios} from 'axios';
+import toast from 'react-hot-toast';
+import { log } from 'node:console';
 
 
 
 export default function signup() {
+    const router = useRouter();
+    const [buttonDisabled, SetButtonDisabled] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [user, setUser] = React.useState({
         email: '',
         username: '',
         password: ''
     });
     const onsignup = async () => {
-
+        try {
+            
+        } catch (error:any) {
+            console.log("Error in signup: ", error);
+            toast.error(error.message);
+        }finally {
+            setLoading
+        }
     }
+    useEffect(() => {
+        if(user.email.length > 0 && user.username.length > 0 && user.password.length >0){
+            SetButtonDisabled(false);
+        }else{
+            SetButtonDisabled(true);
+        }
+    }, [user]);
+
 
     return (
         <div className="bg-gray-900 flex flex-col items-center justify-center min-h-screen text-white">
         <div className="bg-gray-600 p-6 rounded-lg shadow-lg w-90">
-            <h1 className="text-center text-2xl my-3">Signup Page</h1>
+            <h1 className="text-center text-2xl my-3">{loading ? "Processing": "Signup Page"}</h1>
         <p className="mb-1 text-center text-blue-200">Please enter your credentials to sign up.</p>
         {/* Add your login form here */}
         <form className=''> 
@@ -60,7 +80,7 @@ export default function signup() {
                 type="submit" 
                 onClick={onsignup}
                 className="bg-green-500 rounded-lg border-0 p-2 px-5 m-2 hover:bg-green-700">
-                Submit</button>
+                {buttonDisabled ? "No Signup" : "Signup"}</button>
             </div>
         </form>
                 <div className='flex justify-center mt-2'>
